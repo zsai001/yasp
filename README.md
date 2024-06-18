f7f80defbd08075ac0389960db9bbe8f3fca503a
zsai001 <zsai001@qq.com>
2024-06-18 15:39:18 +0800

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+# yasp
+# yasp
---
+++ start.sh
@@ -0,0 +1,7 @@
+echo "# yasp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/zsai001/yasp.git
+git push -u origin main
